b2390ac0d0d4b7bd991755cb2f7b37b38025f448
josuerx12 <josueaze12@gmail.com>
2023-04-11 13:31:29 -0300

    projeto pokedex finalizado para pokemons da 1 G.

---
+++ js/index.js
@@ -0,0 +1,46 @@
+const pokemonList = document.querySelector('.pokemons')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 10
+let offset = 0
+const maxRecords = 151
+
+function loadPokemonItens(offset, limit) {
+    
+
+    pokeApi.getPokemons(offset, limit).then ((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.pokemonNumber}</span>
+            <span class="name">${pokemon.name}</span>
+
+        <div class="detail">
+            <ol class="types">
+                ${pokemon.types.map((type) => `<li class="type ${type}"> ${type} </li>`).join('')}
+            </ol>
+
+            <img src="${pokemon.photo}" alt="${pokemon.name}">
+        </div>
+        
+        </li>
+    `).join('')
+
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+
+    const qtdRecordsWithNextPage = offset + limit
+
+    if(qtdRecordsWithNextPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    }
+
+    loadPokemonItens(offset, limit)
+})
